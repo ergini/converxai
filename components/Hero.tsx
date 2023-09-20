1ede2c22374ff3e94bd3883f14b1cbc37e812962
Ergo <ergin@tetbit.com>
2023-09-20 09:21:39 +0200

    Changes at hero section - homepage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,8 @@ import { Container } from "@mui/material";
 import { Button } from "./ui/button";
 import styled from "@emotion/styled";
 import { BotIcon, BrainCircuitIcon, FilePieChartIcon, HelpCircleIcon, PartyPopperIcon, ScatterChartIcon, TrendingUpIcon } from "lucide-react";
+import { useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const processes = [
     {
@@ -113,6 +115,32 @@ const Content = styled.div`
             color: #845ec2;
         }
     }
+
+    .gradient {
+        position: absolute;
+        right: 0;
+        align-items: center;
+        justify-content: center;
+        transform: translate(-50%, 40%);
+        z-index: -1;
+        width: 50vw;
+        height: 50vh;
+        opacity: 0.3;
+        filter: blur(50px);
+        border-radius: 30%;
+        background-image: linear-gradient(to right top, #845ec2, #9d64d3, #b86ae3, #d46ef2, #f272ff);
+    }
+
+    .white-square-grid {
+        position: absolute;
+        left: 0;
+        z-index: -1;
+        width: 100%;
+        height: 100vh;
+        opacity: 0.4;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40'%3E%3Crect width='40' height='40' fill='%23fff' /%3E%3Crect x='50%' width='1' height='100%' fill='%23ddd' /%3E%3Crect y='50%' width='100%' height='1' fill='%23ddd' /%3E%3C/svg%3E%0A");
+        background-size: 20px 20px;
+    }
 `;
 
 const VideoWrapper = styled.div`
@@ -133,8 +161,10 @@ const VideoWrapper = styled.div`
 const ProcessWrapper = styled.div`
     display: flex;
     flex-direction: row;
+    justify-content: center;
+    align-items: center;
     flex-wrap: wrap;
-    margin-top: 60px;
+    margin-top: 50px;
     font-family: 'Poppins', sans-serif;
 `;
 
@@ -144,13 +174,15 @@ const Process = styled.div`
     align-items: start;
     justify-content: start;
     font-family: 'Poppins', sans-serif;
-    width: 350px;
+    max-width: 360px;
     height: 100%;
     border-radius: 10px;
     background-color: #fefefe;
     padding: 20px;
     border: 1px solid #ddd;
     margin-left: 20px;
+    cursor: default;
+    transition: all 0.3s ease-in-out;
 
     h1{
         font-size: 20px;
@@ -168,9 +200,21 @@ const Process = styled.div`
         margin-top: 10px;
         text-align: left;
     }
+
+    &:hover {
+        transform: translate(0, -10px);
+    }
 `;
 
 export default function Hero() {
+    const [smallDevice, setSmallDevice] = useState(false);
+
+    const matches = useMediaQuery('(max-width:900px)');
+
+    useEffect(() => {
+        setSmallDevice(matches);
+    }, [matches]);
+
     return (
         <Container>
             <Title>
@@ -206,60 +250,95 @@ export default function Hero() {
                 </video>
             </VideoWrapper>
             <Content>
+                <div className="gradient"></div>
+                <div className="white-square-grid"></div>
                 <div className="line"></div>
                 <p>
                     The Process
                 </p>
-                <h1>
+                <h1 className="bg-bl">
                     How do we do it?
                 </h1>
                 <p className="steps">
                     After we receive your request and we make a deal, these are steps that we follow to bring you <span>a masterpiece for your business.</span>
                 </p>
-                <ProcessWrapper>
-                    {processes.slice(0, 1).map((process) => (
-                        <Process key={process.id}>
-                            <FilePieChartIcon size={35} color="#333" />
-                            <h1>{process.title}</h1>
-                            <p>{process.description}</p>
-                        </Process>
-                    ))}
-                    {processes.slice(1, 2).map((process) => (
-                        <Process style={{ marginTop: 30 }} key={process.id}>
-                            <BrainCircuitIcon size={35} color="#333" />
-                            <h1>{process.title}</h1>
-                            <p>{process.description}</p>
-                        </Process>
-                    ))}
-                    {processes.slice(2, 3).map((process) => (
-                        <Process style={{ marginTop: 60 }} key={process.id}>
-                            <BotIcon size={35} color="#333" />
-                            <h1>{process.title}</h1>
-                            <p>{process.description}</p>
-                        </Process>
-                    ))}
-                    {processes.slice(3, 4).map((process) => (
-                        <Process key={process.id}>
-                            <ScatterChartIcon size={35} color="#333" />
-                            <h1>{process.title}</h1>
-                            <p>{process.description}</p>
-                        </Process>
-                    ))}
-                    {processes.slice(4, 5).map((process) => (
-                        <Process style={{ marginTop: 30 }} key={process.id}>
-                            <TrendingUpIcon size={35} color="#333" />
-                            <h1>{process.title}</h1>
-                            <p>{process.description}</p>
-                        </Process>
-                    ))}
-                    {processes.slice(5, 6).map((process) => (
-                        <Process style={{ marginTop: 60, background: 'rgba(132, 94, 194, 0.7)' }} key={process.id}>
-                            <PartyPopperIcon size={35} color="white" />
-                            <h1 style={{ color: 'white' }}>{process.title}</h1>
-                            <p style={{ color: 'white' }}>{process.description}</p>
-                        </Process>
-                    ))}
-                </ProcessWrapper>
+                {!smallDevice ? (
+                    <ProcessWrapper>
+                        {processes.slice(0, 1).map((process) => (
+                            <Process className="backdrop-blur-md" key={process.id}>
+                                <FilePieChartIcon size={35} color="#333" />
+                                <h1>{process.title}</h1>
+                                <p>{process.description}</p>
+                            </Process>
+                        ))}
+                        {processes.slice(1, 2).map((process) => (
+                            <Process className="backdrop-blur-md" style={{ marginTop: 30 }} key={process.id}>
+                                <BrainCircuitIcon size={35} color="#333" />
+                                <h1>{process.title}</h1>
+                                <p>{process.description}</p>
+                            </Process>
+                        ))}
+                        {processes.slice(2, 3).map((process) => (
+                            <Process className="backdrop-blur-md" style={{ marginTop: 60 }} key={process.id}>
+                                <BotIcon size={35} color="#333" />
+                                <h1>{process.title}</h1>
+                                <p>{process.description}</p>
+                            </Process>
+                        ))}
+                        {processes.slice(3, 4).map((process) => (
+                            <Process className="backdrop-blur-md" key={process.id}>
+                                <ScatterChartIcon size={35} color="#333" />
+                                <h1>{process.title}</h1>
+                                <p>{process.description}</p>
+                            </Process>
+                        ))}
+                        {processes.slice(4, 5).map((process) => (
+                            <Process className="backdrop-blur-md" style={{ marginTop: 30 }} key={process.id}>
+                                <TrendingUpIcon size={35} color="#333" />
+                                <h1>{process.title}</h1>
+                                <p>{process.description}</p>
+                            </Process>
+                        ))}
+                        {processes.slice(5, 6).map((process) => (
+                            <Process className="backdrop-blur-md bg-purple-700/60" style={{ marginTop: 60 }} key={process.id}>
+                                <PartyPopperIcon size={35} color="white" />
+                                <h1 style={{ color: 'white' }}>{process.title}</h1>
+                                <p style={{ color: 'white' }}>{process.description}</p>
+                            </Process>
+                        ))}
+                    </ProcessWrapper>
+                ) : (
+                    <ProcessWrapper>
+                        {processes.slice(0, 5).map((process) => (
+                            <Process style={{ marginBottom: 20 }} key={process.id}>
+                                <FilePieChartIcon size={35} color="#333" />
+                                <h1>{process.title}</h1>
+                                <p>{process.description}</p>
+                            </Process>
+                        ))}
+                        {processes.slice(5, 6).map((process) => (
+                            <Process style={{ background: 'rgba(132, 94, 194, 0.7)' }} key={process.id}>
+                                <PartyPopperIcon size={35} color="white" />
+                                <h1 style={{ color: 'white' }}>{process.title}</h1>
+                                <p style={{ color: 'white' }}>{process.description}</p>
+                            </Process>
+                        ))}
+                    </ProcessWrapper>
+                )}
+            </Content>
+            <Content>
+                <div className="gradient"></div>
+                <div className="white-square-grid"></div>
+                <div className="line"></div>
+                <p>
+                    The Process
+                </p>
+                <h1 className="bg-bl">
+                    How do we do it?
+                </h1>
+                <p className="steps">
+                    After we receive your request and we make a deal, these are steps that we follow to bring you <span>a masterpiece for your business.</span>
+                </p>
             </Content>
         </Container>
     )
